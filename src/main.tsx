cae99fcc80fba4582214cf540021c07fc93bc02b
LuissMello <lhnmello@hotmail.com>
2026-02-23 07:33:37 -0300

    ajuste

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-﻿import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+﻿import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        {/* ✅ USA O BASE DO VITE */}
         <BrowserRouter basename={import.meta.env.BASE_URL}>
             <App />
         </BrowserRouter>
     </React.StrictMode>
-);
+)
